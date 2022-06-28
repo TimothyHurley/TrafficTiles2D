e3c92e8ff320aa6a89a7b9c46653318d53b8f8c0
TimothyHurley <timothyhurley.threesix@gmail.com>
2022-06-28 14:36:37 +1000

    Added a Timer
    That is all.

---
+++ TrafficTiles/Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Slider timer; // Timer in canvas.
+
+    public float maxTime = 10f; // Maximum value of timer.
+    public float remainingTime = 10f; // Current value of timer. Starts equal to maxTime.
+    public float addTime = 7f; // Value added to timer.
+
+
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            timer.value = remainingTime / maxTime;
+        }
+
+        /*else
+        {
+            SceneManager.LoadScene("");
+        }*/
+    }
+
+    public void IncreaseTimerValue()
+    {
+        remainingTime = remainingTime + addTime;
+
+        if (remainingTime > maxTime)
+        {
+            remainingTime = maxTime;
+        }
+    }
+}
